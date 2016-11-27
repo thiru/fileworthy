9e3814f3d01e7fe0b62576e32cd6ec857e616c48
Thiru Thirunavukarasu <thiru0130@gmail.com>
2016-11-27 13:10:33 -0500

    Startup script to use GNU screen

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-ros -Q -l start.lisp wait
+# Starts the web server in a screen session:
+# * detached by default
+# * name of session is "fwweb"
+# * session will not die even if sbcl process ends
+
+screen -dmS fwweb sh -c 'sbcl --load start.lisp; exec zsh'

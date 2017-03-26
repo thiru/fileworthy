551af19270835059494da4a86400d3101fc2f72e
Thiru Thirunavukarasu <thiru0130@gmail.com>
2017-03-26 16:23:25 -0400

    Production mode declaim in start.lisp

--- start.lisp
+++ start.lisp
@@ -1,7 +1,9 @@
-;;;; Launches website with Swank
+;;;; Starts the website with Swank in production mode
 
 (ql:quickload :swank)
 (swank:create-server :port 4005 :dont-close t)
+
+(declaim (optimize (safety 1) (speed 3)))
 
 (ql:quickload :fileworthy)
 (fileworthy:start-app :debug nil)

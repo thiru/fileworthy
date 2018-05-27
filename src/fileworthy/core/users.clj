eb4d189e9054cbd3bf3517b1eb41c4d74e14b94e
Thiru Thirunavukarasu <thiru0130@gmail.com>
2018-05-27 12:33:12 -0400

    Users backend

--- src/fileworthy/core/users.clj
+++ src/fileworthy/core/users.clj
@@ -7,6 +7,7 @@
             [clojure.string :as str]
             [clojure.pprint :refer :all]
 
+            [glu.ednfilestore :as efs]
             [glu.logging :refer :all]
             [glu.reporting :refer :all]
             [glu.utils :refer :all]
@@ -14,7 +15,32 @@
             [fileworthy.app :as app]
             [fileworthy.web.routes.template :refer :all]))
 
+(def file "data/users.edn")
+
 (defn get-all
-  "TODO: docs"
-  [info-map]
-  {:id 1 :name "Thiru"})
+  "Get all users.
+
+  Returns:
+  * A map of users keyed by their unique username or,
+  * `nil` if there are no registered users"
+  []
+  (efs/load file))
+
+(defn get-one
+  "Get the first user matching the specified criteria.
+
+  * `filters`
+    * A map specifying filtering criteria
+    * E.g. `{:username \"bob\"}`
+
+  Returns:
+  * A map specifying a user, if found
+  * `nil` if there were no matches"
+
+  [filters]
+
+  (let [all-users (get-all)]
+    (when (non-empty? all-users)
+      (cond
+        (:username filters)
+        (get all-users (:username filters))))))

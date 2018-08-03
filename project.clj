d107782b31317370bb389c0a46c1d110f38cf142
Thiru Thirunavukarasu <thiru0130@gmail.com>
2018-08-03 15:54:30 -0400

    Leiningen config leverages deps.edn

--- project.clj
+++ project.clj
@@ -3,94 +3,11 @@
   :url "https://github.com/thiru/fileworthy"
   :license {:name "GPLv3"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies
-  [;; ## Core Domain
-
-    ;; Clojure on JVM:
-   [org.clojure/clojure "1.9.0"]
-
-   ;; Date/time library:
-   [clojure.java-time "0.3.1"]
-
-   ;; Converts between different string case conventions:
-   [camel-snake-kebab "0.4.0"]
-
-   ;; ## Web Server
-
-   ;; Web server abstraction based on middleware:
-   [ring/ring-core "1.6.3"]
-   ;; Development middleware (e.g. middleware to auto-reload handlers):
-   [ring/ring-devel "1.6.3"]
-   ;; In-process web server:
-   [ring/ring-jetty-adapter "1.6.3"]
-   ;; Sensible middleware defaults for Ring:
-   [ring/ring-defaults "0.3.1"]
-   ;; Simpler HTTP response handling (replaces `ring.util.response`):
-   [metosin/ring-http-response "0.9.0"]
-   ;; Routing library for Ring
-   [compojure "1.6.0"]
-   ;; Generate HTML from clojure data structures:
-   [hiccup "1.0.5"]
-   ;; JSON encoding/decoding:
-   [cheshire "5.8.0"]
-   ;; Better exception reporting middleware:
-   [prone "1.1.4"]
-   ;; Security library:
-   [buddy "2.0.0"]
-
-   ;; ## Web Client
-
-   ;; React wrapper:
-   [reagent "0.8.1"]
-
-   ;; ## Domain-agnostic Infrastructure (config, debugging, logging, etc.)
-
-   ;; Config loader:
-   [aero "1.1.3"]
-
-   ;; Tracing tools during development:
-   [org.clojure/tools.trace "0.7.9"]
-
-   ;; The following libraries revolve around SLF4J (Simple Logging Facade for
-   ;; Java). SLF4J serves as a simple facade or abstraction for various
-   ;; logging frameworks (e.g. log4j, java.util.logging, logback, etc.),
-   ;; allowing the user to plug in the desired logging framework at
-   ;; *deployment* time.
-   ;;
-   ;; These are needed in order to get logs from Jetty.
-
-   ;; SLF4J binding for Jakarta Commons Logging:
-   [org.slf4j/jcl-over-slf4j "1.7.25"]
-   ;; SLF4J binding for java.util.logging:
-   [org.slf4j/jul-to-slf4j "1.7.25"]
-   ;; SLF4J binding for log4j:
-   [org.slf4j/log4j-over-slf4j "1.7.25"]
-   ;; SLF4J native binding:
-   [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
-
-   ;; ## Tooling
-
-   ;; Clojure *network* REPL server/client:
-   [org.clojure/tools.nrepl "0.2.13"]
-   ;; nREPL middleware for CIDER. This is used by Vim's Fireplace plugin:
-   [cider/cider-nrepl "0.16.0"]
-   ;; nREPL middleware needed to communicate with vim-fireplace:
-   [cider/piggieback "0.3.6"]
-   ;; Command-line interface helper:
-   [org.clojure/tools.cli "0.3.5"]
-   ;; Nicer REPL (Clojure):
-   [com.bhauman/rebel-readline "0.1.4"]
-   ;; Nicer REPL (ClojureScript):
-   [com.bhauman/rebel-readline-cljs "0.1.4"]
-   ;; ANSI colours for terminal:
-   [clansi "1.0.0"]
-   ;; Literate programming-ish documentation
-   [lein-marginalia "0.9.1"]
-   ;; Namespace change tracker:
-   [ns-tracker "0.3.1"]
-   ;; File-system watcher:
-   [hawk "0.2.11"]]
-  :plugins [[lein-marginalia "0.9.1"]]
+  :plugins [[lein-marginalia "0.9.1"]
+            [lein-tools-deps "0.4.1"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]
+                           :clojure-executables ["/usr/bin/clojure"]}
   :main ^:skip-aot user
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
